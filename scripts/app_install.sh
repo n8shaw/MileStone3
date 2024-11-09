ac37c958227cb031cc57f5d2929ee1b8e92eb005
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 12:48:58 -0700

    Update app_install.sh

--- scripts/app_install.sh
+++ scripts/app_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+chmod +x /home/ec2-user/MileStone3/scripts/*.sh
 cd /home/ec2-user/MileStone3
 export NVM_DIR="/home/ec2-user/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-npm install 
+npm install
