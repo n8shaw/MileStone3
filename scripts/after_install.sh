cc735beed2382623b0efb568aaf6a5d5f38a8732
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 12:21:21 -0700

    Update and rename after_install.sh to app_install.sh

--- scripts/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "DB_HOST=${DB_HOST}" >> /home/ec2-user/MileStone3/.env
-echo "DB_USER=${DB_USER}" >> /home/ec2-user/MileStone3/.env
-echo "DB_PASS=${DB_PASS}" >> /home/ec2-user/MileStone3/.env
-echo "DB_NAME=${DB_NAME}" >> /home/ec2-user/MileStone3/.env
