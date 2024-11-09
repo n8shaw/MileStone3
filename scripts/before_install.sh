bb649106aed69112d5bf9a76d7d6fa9c00747511
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 12:17:38 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,10 +3,18 @@
 # Make sure we're in the correct directory
 cd /home/ec2-user/MileStone3
 
-# Remove any existing node_modules (to ensure a fresh install)
-sudo rm -rf /home/ec2-user/MileStone3/package-lock.json
-sudo rm -rf /home/ec2-user/MileStone3/*
+# Remove the package-lock.json file if it exists
+if [ -f "package-lock.json" ]; then
+  sudo rm -f package-lock.json
+fi
 
+# Remove the existing node_modules directory
+if [ -d "node_modules" ]; then
+  sudo rm -rf node_modules
+fi
+
+# Optional: Remove other files or directories as needed
+# sudo rm -rf some_other_file_or_dir
 
 # Install project dependencies
 sudo npm install
