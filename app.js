d2673ab503c963eea66236684a2693fd5f6ea0d1
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 11:31:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const knex = require('knex');
 const app = express();
 
 // Configure knex to use environment variables
-const db = knex({
+const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: 'database-1.c1iugckwg6rp.us-east-2.rds.amazonaws.com',  // Strings must be wrapped in quotes
+    user: 'admin',  // Strings must be wrapped in quotes
+    password: 'Password1!',  // Strings must be wrapped in quotes
+    database: 'donuts',  // Strings must be wrapped in quotes
     port: 3306
   }
 });
+
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
