dc5560b962cc1294f988e33c18d7cb3aa107ce6e
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 10:31:51 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,13 +4,15 @@
 mkdir -p /home/ec2-user/MileStone3
 cd /home/ec2-user/MileStone3
 
-# Install Node.js and npm if not already installed
-if ! command -v npm &> /dev/null; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install node
-fi
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install a compatible version of Node.js
+nvm install 18
+
+# Use Node.js v18
+nvm use 18
 
 # Install project dependencies
 npm install
