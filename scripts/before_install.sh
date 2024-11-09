82bfc63e25e6d2067e2fda009c5cb2024cfc0042
Nate Shaw <119529291+n8shaw@users.noreply.github.com>
2024-11-09 11:01:59 -0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,12 +4,11 @@
 mkdir -p /home/ec2-user/MileStone3
 cd /home/ec2-user/MileStone3
 
-sudo yum install -y git
+# If the directory is not empty, remove existing files
+rm -rf *
+
 # Clone the repository (replace with your actual repo URL)
 git clone https://github.com/n8shaw/MileStone3.git .
-
-# Checkout the specific branch (replace with your branch name)
-
 
 # Install Node.js
 export NVM_DIR="$HOME/.nvm"
@@ -19,3 +18,4 @@ nvm use 16
 
 # Install dependencies
 npm install
+
